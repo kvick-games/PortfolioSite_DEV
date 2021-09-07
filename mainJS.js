45120bd310fa7dbd392977220fc291abcae7cce5
kvickz <mkn665@yahoo.com>
2021-09-06 23:29:51 -0400

    added a sidebar

--- mainJS.js
+++ mainJS.js
@@ -3,7 +3,7 @@
 function addSecondButton()
 {
     let eButt = document.createElement("button");
-    eButt.innerHTML = "Button TWO";
+    eButt.innerHTML = "Wipeout";
     
     let eFooter = document.querySelector("footer");
     if (eFooter)
@@ -25,11 +25,87 @@ eButter.onclick = onButtPressed;
 let eSpamBox = document.getElementById("spambox");
 if (eSpamBox)
 {
-    for(let i = 0; i < 50; ++i)
+    for(let i = 0; i < 225; ++i)
     {
-        let eNew = document.createElement("span");
+        let eNew = document.createElement("p");
         eNew.innerHTML = "SpamBoxContent".toUpperCase();
         eNew.className = "spambox";
+        //eNew.style.display = "inline-block";
+        eNew.style.width = "115px";
         eSpamBox.appendChild(eNew);
     }
+}
+
+//==============================
+//Nav bar
+
+let offsetLeft = 350;
+let navBarLeftOffset = 50;
+let navBarExpandedLeftOffset = offsetLeft + navBarLeftOffset;
+let navBarExpanded = false;
+
+function toggleOpenNav()
+{
+    if (navBarExpanded)
+    {
+        closeNav();
+    }
+    else
+    {
+        openNav();
+    }
+}
+
+function openNav()
+{
+    navBarExpanded = true;
+
+    let eSidebar = document.getElementById("mainSideBar");
+    if (eSidebar)
+    {
+        eSidebar.style.width = offsetLeft + "px";
+    }
+    else
+    {
+        console.error("Failed to get sidebar");
+    }
+
+    let eMainContent = document.getElementById("mainContent");
+    if (eMainContent)
+    {
+        eMainContent.style.width = offsetLeft + "px";
+    }
+    else
+    {
+        console.error("Failed to get maincontent");
+    }
+
+    document.getElementById("navIcon").style.left = navBarExpandedLeftOffset + "px";
+}
+
+function closeNav()
+{
+    navBarExpanded = false;
+
+    let eSidebar = document.getElementById("mainSideBar");
+    if (eSidebar)
+    {
+        eSidebar.style.width = 0 + "px";
+    }
+    else
+    {
+        console.error("Failed to get sidebar");
+    }
+
+    let eMainContent = document.getElementById("mainContent");
+    if (eMainContent)
+    {
+        eMainContent.style.width = 0 + "px";
+    }
+    else
+    {
+        console.error("Failed to get maincontent");
+    }
+
+    document.getElementById("navIcon").style.left = navBarLeftOffset + "px";
 }
